33716e7e479488a04b57c7a5282602d02d7060b4
zzc19890415 <zczhou@connect.hku.hk>
2020-02-18 14:54:27 -0600

    Update draw_metabolic_network.R

--- draw_metabolic_network.R
+++ draw_metabolic_network.R
@@ -1,7 +1,8 @@
 # Patricia Tran
-# November 27, 2019
+# December 4, 2019
 
 # Generate a network plot with reactions as node, and edges are MAGs, and are colored by taxonomic group.
+
 userprefs <- commandArgs(trailingOnly = TRUE)
 R_input_table <- userprefs[1] # Path to folder with all the summary files Energy_flow_input.txt is.
 plots.folder.path <- userprefs[2] # Name of new directory to make to store things
@@ -41,6 +42,12 @@ library(igraph)
 
 library(tidyverse)
 
+library(tidygraph)
+
+my_graph <- table[,c(2,3,4,5)] %>% 
+  graph_from_data_frame()
+
+deg <- degree(my_graph, mode="all")
 
 # this generates the whole community plot
 community.plot <- table[,c(2,3,4,5)] %>% 
@@ -48,11 +55,13 @@ community.plot <- table[,c(2,3,4,5)] %>%
   ggraph(layout = "linear",circular = TRUE) +
   geom_edge_arc(alpha = .25, 
                  aes(width = Coverage.value.average., color=as.factor(Taxonomic.Group))) +
-  geom_node_point(color = "black", size = 2) +
+  geom_node_point(color = "black", size = 0.02*deg, alpha=0.75) +
   geom_node_text(aes(label = name),  color="black", repel = TRUE)+
-  theme_graph()+
+  #theme_graph()+
   labs(title = 'Metabolic connections within dataset', 
        subtitle = 'No scaling')
+
+#community.plot
 
 plot.name <- paste0(network.plots.folder,"/CommunityPlot.PDF")
 print(plot.name)
@@ -94,6 +103,4 @@ for (i in 1:length(unique.taxo.groups)){
   print(i)
   
 }
-
-
 
