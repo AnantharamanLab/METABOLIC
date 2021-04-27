35647040d0dd68b2acf28f4381f2df458d105dc6
tin6150 <tin@lunaria>
2021-04-26 22:29:29 -0700

    docker containerization - draft from tin6150//e93f1e8

--- run_to_setup.sh
+++ run_to_setup.sh
@@ -1,6 +1,6 @@
 tar zxvf Accessory_scripts.tgz
 tar zxvf METABOLIC_hmm_db.tgz
-tar zxvf METABOLIC_template_and_database.tgz
+tar zxvf METABOLIC_temp_and_db.tgz
 tar zxvf Motif.tgz
 mkdir kofam_database  
 cd kofam_database  
@@ -8,7 +8,6 @@ wget -c ftp://ftp.genome.jp/pub/db/kofam/ko_list.gz
 wget -c ftp://ftp.genome.jp/pub/db/kofam/profiles.tar.gz  
 gzip -d ko_list.gz  
 tar xzf profiles.tar.gz; rm profiles.tar.gz  
-mv ../All_Module_KO_ids.txt profiles
 cd profiles  
 cp ../../Accessory_scripts/batch_hmmpress.pl ./  
 perl batch_hmmpress.pl
@@ -24,6 +23,4 @@ cd MEROPS
 wget ftp://ftp.ebi.ac.uk/pub/databases/merops/current_release/pepunit.lib
 perl ../Accessory_scripts/make_pepunit_db.pl
 cd ../
-gdown https://drive.google.com/uc?id=1JQJpw_elM4IyGo_BIfioy8XnmqgoN-Iw
-tar zxvf METABOLIC_test_files.tgz
-rm *.tgz
+tar zxvf 5_genomes_test.tgz
